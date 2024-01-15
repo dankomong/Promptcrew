6400291f11f040b77d4cf07b4f7d7d9a1619748e
Daniel <dankomong@gmail.com>
2024-01-14 20:14:37 -0500

    fix typo in setting up user id

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -25,7 +25,7 @@ const CreatePrompt = () => {
                 method: 'POST',
                 body: JSON.stringify({
                     prompt: post.prompt,
-                    userId: session?.user_id,
+                    userId: session?.user.id,
                     tag: post.tag
                 })
             })
